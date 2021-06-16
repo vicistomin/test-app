f342d868d530c02ee31607198cfab48bb5296de5
Vic Istomin <vic.istomin@gmail.com>
2021-06-16 13:34:18 +0300

    feat: added support for Redux DevTools Extension

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ import { createStore } from 'redux';
 import { rootReducer } from './services/reducers';
 import { Provider } from 'react-redux';
 
-const store = createStore(rootReducer); 
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  ); 
 
 ReactDOM.render(
   <React.StrictMode>
