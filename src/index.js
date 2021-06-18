8869b39a51a690c2077045702ad0f3d5725295f3
Vic Istomin <vic.istomin@gmail.com>
2021-06-18 20:57:55 +0300

    feat: added thunk & rewrited Extension connection

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ import './index.css';
 
 import App from './components/app/app';
 
-import { createStore } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import { rootReducer } from './services/reducers';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  ); 
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose; 
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+const store = createStore(rootReducer, enhancer); 
 
 ReactDOM.render(
   <React.StrictMode>
